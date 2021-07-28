1d45ee215b196cfeb8e05d8b9dac21efa023066f
Joao Rodrigues <jrodrigues@overstock.com>
2021-07-28 17:15:43 +0100

    Error handling

--- lib/cards.ex
+++ lib/cards.ex
@@ -49,6 +49,10 @@ defmodule Cards do
 
   def load(filename) do
     {status, binary} = File.read(filename)
-    :erlang.binary_to_term(binary)
+
+    case status do
+      :ok -> :erlang.binary_to_term(binary)
+      :error -> "That file doesn't exist"
+    end
   end
 end
