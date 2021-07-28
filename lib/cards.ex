68a6004c2fb3117ad5197eb5de9310cc275cc878
Joao Rodrigues <jrodrigues@overstock.com>
2021-07-28 14:40:35 +0100

    Initial commit

---
+++ lib/cards.ex
@@ -0,0 +1,26 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for `Cards`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def hello2 do
+    "hi there"
+  end
+
+  def create_deck do
+    ["Ace", "Two", "Three"]
+  end
+end
