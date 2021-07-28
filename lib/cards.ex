bd1c560b5763620512d4b6d2f0a148584c9f1481
Joao Rodrigues <jrodrigues@overstock.com>
2021-07-28 15:12:06 +0100

    Using comprehension with two lists

--- lib/cards.ex
+++ lib/cards.ex
@@ -24,13 +24,10 @@ defmodule Cards do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-    cards = for value <- values do
-      for suit <- suits do
-        "#{value} of #{suit}"
-      end
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
     end
 
-    List.flatten(cards)
   end
 
   def shuffle(deck) do
